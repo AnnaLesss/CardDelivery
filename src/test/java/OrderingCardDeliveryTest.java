be036f62785343c0ef26daf724e9d9de80b1ab4f
AnnaLesss <klatter@mail.ru>
2022-10-12 12:28:39 +0300

    new

--- src/test/java/OrderingCardDeliveryTest.java
+++ src/test/java/OrderingCardDeliveryTest.java
@@ -1,41 +1,22 @@
 import com.codeborne.selenide.Condition;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.exactText;
+
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class OrderingCardDeliveryTest {
 
-    private WebDriver driver;
-
-    @BeforeAll
-    static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\annbo\\IdeaProjects\\CardDelivery\\driver\\win\\chromedriver.exe");
-    }
-
     @BeforeEach
     void setUp() {
-        driver = new ChromeDriver();
         open("http://localhost:9999/");
-    }
-
-    @AfterEach
-    void tearDown() {
-        driver.quit();
-        driver = null;
     }
 
     String generateDate(int days) {
@@ -51,6 +32,8 @@ public class OrderingCardDeliveryTest {
         $("[data-test-id=phone] input").setValue("+79532223322");
         $("[data-test-id=agreement]").click();
         $$("button").findBy(Condition.exactText("Забронировать")).shouldBe(visible, Duration.ofSeconds(15)).click() ;
-        $("[data-test-id=notification]").find(generateDate(3)).shouldBe(visible, Duration.ofSeconds(5));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + generateDate(3)), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
     }
 }
